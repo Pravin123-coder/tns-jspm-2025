70f9af0fc9050bc5291ed443ce30053851c7fdf8
ghola <ghola@LAPTOP-HFLBRK40>
2025-08-01 22:51:49 +0530

    cfbgf

---
+++ firstnew/src/com/firstnew/character.java
@@ -0,0 +1,11 @@
+package com.firstnew;
+
+public class character {
+	public static void main(String[] args) {
+		char ch='a';
+		System.out.println(ch);
+		char ch1=65;
+		System.out.print(ch1);
+		}
+
+}
